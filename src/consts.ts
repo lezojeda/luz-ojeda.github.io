8ff53dd15dc594a5dde1e99c4d65ea3ed801396e
Lucas Ezequiel Ojeda <luzojeda@proton.me>
2025-05-24 20:06:33 -0300

    shorten site title on mobile

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Lucas Ezequiel Ojeda";
+export const MOBILE_SITE_TITLE = "Lucas E. Ojeda";
 export const SITE_DESCRIPTION =
 	"The personal web page of a web developer, sometimes cook and bookworm from Buenos Aires, Argentina";
