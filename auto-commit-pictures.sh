bde0ae7f12f748b0b3ea8fcb893a8f457cc01106
Lucas Ezequiel Ojeda <lezojeda@proton.me>
2026-05-16 18:50:41 -0300

    add auto commit

---
+++ auto-commit-pictures.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export HOME="/home/lezojeda"
+export PATH="/usr/bin:/bin"
+export GIT_SSH_COMMAND="ssh -i /home/lezojeda/.ssh/github -o StrictHostKeyChecking=no"
+
+cd /home/lezojeda/repositories/luz-ojeda.github.io
+
+if [[ -n $(/usr/bin/git status -s src/content/pictures/) ]]; then
+    /usr/bin/git add src/content/pictures/
+    /usr/bin/git commit -m "Auto-commit pictures: $(date +'%Y-%m-%d %H:%M')"
+    /usr/bin/git push origin master
+    echo "$(date): pushed"
+else
+    echo "$(date): no changes"
+fi
